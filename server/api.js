c4b767c4e262e5bc068ab7a44414877984e6cc0b
Love Holmgren <143357495+hevolx@users.noreply.github.com>
2026-07-19 19:18:38 +0200

    feat(api): add PUT /api/minions/:id route
    Add an endpoint to update an existing minion by id, using
    updateInstanceInDatabase to persist the change and responding
    404 when the minion does not exist.

--- server/api.js
+++ server/api.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const apiRouter = express.Router();
-const { getAllFromDatabase, getFromDatabaseById } = require('./db');
+const { getAllFromDatabase, getFromDatabaseById, updateInstanceInDatabase } = require('./db');
 
 const minions = getAllFromDatabase('minions');
 
+// Get all minions
 apiRouter.get('/minions', (req, res, next) => {
   res.status(200).send(minions);
   next();
 })
 
+// Get a single minion
 apiRouter.get('/minions/:id', (req, res, next) => {
   const foundMinion = getFromDatabaseById('minions', req.params.id);
 
@@ -19,5 +21,17 @@ apiRouter.get('/minions/:id', (req, res, next) => {
     res.status(404).send();
   }
 })
+
+// Update an minion
+apiRouter.put('/minions/:id', (req, res, next) => {
+  const initialMinion = getFromDatabaseById('minions', req.params.id);
+  if (initialMinion) {
+    let updatedMinionInfo = updateInstanceInDatabase('minions', req.body);
+    res.status(201).send(updatedMinionInfo);
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
 
 module.exports = apiRouter;
