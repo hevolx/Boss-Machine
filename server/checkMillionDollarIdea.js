77e6966e1d29c1ddb791bba3bbc4534728135076
Love Holmgren <143357495+hevolx@users.noreply.github.com>
2026-07-19 20:44:24 +0200

    feat(api): implement checkMillionDollarIdea middleware
    compute totalYield from numWeeks * weeklyRevenue and reject with
    400 when either field is missing/invalid or the yield is under
    one million dollars, otherwise call next()

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,14 @@
-const checkMillionDollarIdea = (req, res, next) => { };
+const checkMillionDollarIdea = (req, res, next) => {
+  const { numWeeks, weeklyRevenue } = req.body;
+  const totalYield = Number(numWeeks) * Number(weeklyRevenue);
+  if (!numWeeks || !weeklyRevenue || isNaN(totalYield)) {
+    return res.status(400).send();
+  }
+  if (totalYield < 1000000) {
+    return res.status(400).send();
+  }
+  next();
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
